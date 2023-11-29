345a40edf2f518aab4b4fed85d85ff22a2f4193b
Sandy Obare <obaresandy@gmail.com>
2023-11-29 09:24:23 +0000

    9-print_last_digit.py initial commit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        mod = number % -10 * -1
+    else:
+        mod = number % 10
+    print("{}".format(mod), end="")
+    return mod
+
