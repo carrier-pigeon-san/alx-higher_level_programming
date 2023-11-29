5e336a63016e9de0f8aeff1d1cd004c34eef862d
Sandy Obare <obaresandy@gmail.com>
2023-11-29 03:03:46 +0000

    7-islower.py replaced print() with return value

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
-        print("{} is lower".format(c))
+        return True
     elif ord(c) >= 65 and ord(c) <= 90:
-        print("{} is upper".format(c))
+        return False
