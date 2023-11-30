aee3e3003eac1cb278b02a28ddeb647eb4334f6c
Sandy Obare <obaresandy@gmail.com>
2023-11-30 19:14:10 +0000

    2-args.py initial commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument")
+        print(f"1: {argv[1]}")
+    else:
+        print(f"{len(argv) - 1:d} arguments:")
+        for i in range(len(argv)):
+            if i > 0:
+                print("{}: {}".format(i, argv[i]))
