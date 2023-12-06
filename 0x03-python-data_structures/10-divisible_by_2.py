ad6b43b85418b3820d89d8a1652dfb45d2d195c0
Sandy Obare <obaresandy@gmail.com>
2023-12-06 00:17:43 +0000

    10-divisible_by_2.py new list appended with actual boolean values rather than strings

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:
         if i % 2 == 0:
-            new_list += ["True"]
+            new_list += [True]
         else:
-            new_list += ["False"]
+            new_list += [False]
     return new_list
