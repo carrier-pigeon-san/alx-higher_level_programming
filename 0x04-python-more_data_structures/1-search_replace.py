f14d3e5ffd68beb5233c7537ac4bffd78c3b82da
Sandy Obare <obaresandy@gmail.com>
2023-12-06 18:17:50 +0000

    1-search_replace.py initial commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return my_list
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list += search
+        else:
+            new_list += i
+    return new_list
