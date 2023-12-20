aeab194b1ff0bd62598e65d3dde57f12e51b2af3
Sandy Obare <obaresandy@gmail.com>
2023-12-20 14:43:04 +0000

    4-square.py file initial commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Definition of a class `Square`, that defines a square"""
+
+
+class Square:
+    """
+    Square class defines a square with a private attribute `size`,
+    that is a required integer greater than or equal to zero
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
