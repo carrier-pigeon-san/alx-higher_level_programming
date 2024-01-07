757d8cb530cd924eb5280727d839ae5d22c68c07
Sandy Obare <obaresandy@gmail.com>
2024-01-07 00:17:18 +0000

    6-rectangle.py file pep8 code style conformation

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """a class that defines a Rectangle"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
