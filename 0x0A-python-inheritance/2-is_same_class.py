a2b07fbf0059f6242b7713f034898ca344d4e2c4
Sandy Obare <obaresandy@gmail.com>
2024-01-09 11:43:41 +0000

    2-is_same_class.py file initial commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains is_same_class() function that returns boolean based on whether a
+given object is exactly an instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
