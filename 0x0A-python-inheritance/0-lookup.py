d76f1eb1767ffd14586d0c59f8f8872360be04f8
Sandy Obare <obaresandy@gmail.com>
2024-01-09 10:37:18 +0000

    initial directory commit, 0-lookup.py file initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains a function: lookup() that returns the list of available attributes
+and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    returns a list of available attributes and methods of an object
+    """
+
+    return dir(obj)
