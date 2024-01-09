6120c641eb0597e7059a9ba2aaabd56766f3ff87
Sandy Obare <obaresandy@gmail.com>
2024-01-09 16:58:59 +0000

    6-base_geometry.py file initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
