0b09ff8cbd89c6db3d7d6ba165ccec0f0bbfe7da
Sandy Obare <obaresandy@gmail.com>
2024-01-13 00:09:47 +0000

    10-square.py initial commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module contains class 'Square' that exhibits properties of a square polygon
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class 'Square', superclass 'Rectangle', instantiated with private
+    attribute, 'size' - a positive integer, greater than zero (0)
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """returns the area of a square"""
+
+        return self.__size**2
