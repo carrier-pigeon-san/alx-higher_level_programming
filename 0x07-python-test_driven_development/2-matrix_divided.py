e93ad737cc892bbf491264096e775e82983996b7
Sandy Obare <obaresandy@gmail.com>
2024-01-14 19:24:02 +0000

    0x07-python-test_driven_development task #1 initial commit

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""matrix_divided() function module"""
+
+
+def matrix_divided(matrix, div):
+    """
+    matrix_divided - divides all elements of a matrix
+    @matrix: given matrix
+    @div: value, matrix elements are to be divided by
+    """
+
+    if type(matrix) is list:
+        for item in range(len(matrix)):
+            if type(matrix[item]) is list:
+                if item == 0:
+                    row_size = len(matrix[item])
+                if len(matrix[item]) != row_size:
+                    raise TypeError('Each row of the matrix must have the"\
+                            "same size')
+                for element in matrix[item]:
+                    if type(element) is not int and type(element) is not float:
+                        raise TypeError('matrix must be a matrix"\
+                                "(list of lists) of integers/floats')
+            else:
+                raise TypeError('matrix must be a matrix"\
+                        "(list of lists) of integers/floats')
+    else:
+        raise TypeError('matrix must be a matrix"\
+                "(list of lists) of integers/floats')
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    matrix2 = [[round(j/div, 2) for j in i] for i in matrix]
+
+    return matrix2
