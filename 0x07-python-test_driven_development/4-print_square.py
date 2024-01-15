21364454f84392c7ec785ab701629b102a05fcde
Sandy Obare <obaresandy@gmail.com>
2024-01-15 23:05:54 +0000

    0x07-python-test_driven_development task #3 pycodestyle conformation

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,7 +7,7 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
-    for l in range(size):
-        for w in range(size):
+    for dimension1 in range(size):
+        for dimension2 in range(size):
             print("#", end="")
         print()
