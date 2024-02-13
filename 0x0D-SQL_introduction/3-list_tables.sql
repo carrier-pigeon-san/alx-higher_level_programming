1552dcc76e7ac6e96572709f70c5b323c1440488
Sandy Obare <obaresandy@gmail.com>
2024-02-13 21:03:20 +0000

    3-list_tables.sql script initial commit script to list tables in a database in a MySQL server

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- SQL script that lists tables in a database in a MySQL server
+-- The database name will be passes as an argument of mysql command as such:
+-- cat 3-list_tables.sql | mysql -h localhost -u root -p `mysql`
+SHOW TABLES
