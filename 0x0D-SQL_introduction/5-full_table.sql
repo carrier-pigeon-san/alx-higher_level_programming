416e3d67f7335836c2b242b1502bbb83ba3701fb
Sandy Obare <obaresandy@gmail.com>
2024-02-13 22:45:58 +0000

    5-full_table.sql script initial commit The script prints the full description of the given table in a database in a MySQL server

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- The script prints the full description of the table `first_table` from...
+-- a database in a MySQL server
+-- The database name is passed as an argument of the MySQL command as such:
+-- * cat %-full_table.sql | mysql -h localhost -u root -p <database>
+SHOW CREATE TABLE first_table;
