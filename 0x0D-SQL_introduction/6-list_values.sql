339f9b8aacdb3177fd41ff0d2c2c5ec0e413cad0
Sandy Obare <obaresandy@gmail.com>
2024-02-14 00:01:29 +0000

    6-list_values.sql script initial commit The script lists all of the table  from the database in  server

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- The script lists all rows of a table from a database in a MySQL server
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command as:
+-- * cat 6-list_alues.sql | mysql -h localhost -u root -p <database>
+SHOW INDEX FROM first_table;
