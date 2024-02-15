20a2bafc392f7d8149a07c7b82c8b76a7652a2a1
Sandy Obare <obaresandy@gmail.com>
2024-02-15 08:33:51 +0000

    2-create_read_user.sql The script creates a user user_0d_2 if the user doesn't exit The script creates a database hbtn_0d_2 if the database doesn't exist User user_0d_2 is granted only SELECT privileges to the database hbtn_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- The scripts creates a database hbtn_0d_2 and user user_0d_2
+-- user_0d_2 is granted only SELECT privileges database hbtn_0d_2
+-- user_0d_@ password is set to user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2
+IDENTIFIED BY user_0d_2_pwd;
+GRANT SELECT
+ON hbtn_0d_2.*
+TO user_0d_2
