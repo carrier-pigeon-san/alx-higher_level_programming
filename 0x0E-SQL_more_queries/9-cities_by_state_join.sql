0f1a520ee6b7e14aa6631747b48425b0ecd8ba83
Sandy Obare <obaresandy@gmail.com>
2024-02-15 19:29:58 +0000

    9-cities_by_state_join.sql script initial commit This script lists all cities in the database hbtn_0d_usa Each record displays cities.id - cities.name - states.name Records are sorted in ascending order by the cities.id

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- This script lists all the cities contained the database hbtn_0d_usa
+-- Each record to display cities.id - cities.name - states.name
+-- Results sortes in ascending order by cities.id
+SELECT id, c.name, s.name
+FROM cities c
+JOIN states s
+  ON c.state_id = s.id
