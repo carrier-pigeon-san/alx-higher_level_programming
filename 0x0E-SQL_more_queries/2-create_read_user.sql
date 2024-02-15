c71e90f524341c816e297d2f3205463c5acd6cbf
Sandy Obare <obaresandy@gmail.com>
2024-02-15 08:48:36 +0000

    2-create_read_user.sql CREATE USER statement syntax correction

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,8 +2,8 @@
 -- user_0d_2 is granted only SELECT privileges database hbtn_0d_2
 -- user_0d_@ password is set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2
-IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 ON hbtn_0d_2.*
 TO 'user_0d_2'@'localhost';
