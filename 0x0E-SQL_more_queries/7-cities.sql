ca39a46e35db05806f030a7f39e44a105f5fdf71
Sandy Obare <obaresandy@gmail.com>
2024-02-15 13:59:33 +0000

    7-cities.sql script initial commit This script creates a database 'hbtn_0d_usa' and a table 'cities' within it, if either do not exist cities table fields: * id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE * state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id) * name VARCHAR(256) NOT NULL

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- The script creates database 'hbtn_0d_usa' and table 'cities' in the...
+-- database 'hbtn_0d_usa' on a MySQL server
+-- fields:
+-- * id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE
+-- * state_id INT NOT NULL FOREIGN KEY to id of the states table
+-- * name VARCHAR(256) NOT NULL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id		INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+	state_id	INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
+	name		VARCHAR(256) NOT NULL
+);
+
