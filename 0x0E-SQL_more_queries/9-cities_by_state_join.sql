1343a531ca4b2689c37e0aa3f90146f040831596
Sandy Obare <obaresandy@gmail.com>
2024-02-15 21:20:32 +0000

    9-cities_by_state_join.sql query syntax change

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,7 @@
 -- This script lists all the cities contained the database hbtn_0d_usa
 -- Each record to display cities.id - cities.name - states.name
 -- Results sortes in ascending order by cities.id
-SELECT id, c.name, s.name
-FROM cities c
-JOIN states s
-  ON c.state_id = s.id
-ORDER BY c.id ASC;
+SELECT id, cities.name, states.name
+FROM cities
+  JOIN states
+    ON cities.state_id = states.id ORDER BY id ASC;
