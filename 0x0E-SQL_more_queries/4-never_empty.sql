a21fef1189eeeac411e96e87dd8ae3dc9622dd25
Sandy Obare <obaresandy@gmail.com>
2024-02-15 10:09:34 +0000

    4-never_empty.sql script initial commit The script creates the table 'id_not_null' on a MySQL server fields: * id - INT DEFAULT 1 * name - VARCHAR(256)

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,10 @@
+-- The script creates the table 'id_not_null' on a MySQL server
+-- fields:
+-- * id - INT default value = 1
+-- * name - VARCHAR(256)
+-- the database name wil be passed as an argument in the mysql command as:
+-- cat 4-never_empty.sql | mysql -h localhost -u root -p <database>
+CREATE TABLE IF NOT EXISTS force_name (
+	id	INT DEFAULT 1,
+	name	VARCHAR(256)
+);
