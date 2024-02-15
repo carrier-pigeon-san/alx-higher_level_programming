6660ab6ead67f55c19431f5ba3eab4dac69ace04
Sandy Obare <obaresandy@gmail.com>
2024-02-15 10:11:58 +0000

    4-never_empty.sql CREATE TABLE Statement typo correction

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -4,7 +4,7 @@
 -- * name - VARCHAR(256)
 -- the database name wil be passed as an argument in the mysql command as:
 -- cat 4-never_empty.sql | mysql -h localhost -u root -p <database>
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS id_not_null (
 	id	INT DEFAULT 1,
 	name	VARCHAR(256)
 );
