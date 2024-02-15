184ba1fe330cc334d59174c2252cec832b3b0cc1
Sandy Obare <obaresandy@gmail.com>
2024-02-15 08:07:09 +0000

    1-create_user.sql typo correction in GRANT statement

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,10 +2,8 @@
 -- * has all privileges on the MySQL server
 -- * has the password set to user_0d_1_pwd
 -- The script does not fail if the user already exists
-CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'use_0d_1'@'localhost' 
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost' 
 WITH GRANT OPTION;
