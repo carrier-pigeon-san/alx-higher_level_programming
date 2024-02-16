0957a080a1be2f7bad53cd1345b91119c3ddbfa7
Sandy Obare <obaresandy@gmail.com>
2024-02-16 23:17:13 +0000

    100-not_my_genres.sql query typos rectification

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,11 +2,11 @@
 -- Each record displays the tv_genres.name column
 -- Results are sorted in ascending order by genre name
 SELECT tg.name
-FROM tv_gernres tg
+FROM tv_genres tg
 WHERE NOT EXISTS (
 	SELECT 1
 	FROM tv_show_genres tsg
-	JOIN tv_shows ts ON tsg.shows_id = ts.id
+	JOIN tv_shows ts ON tsg.show_id = ts.id
 	WHERE tsg.genre_id = tg.id AND ts.title = 'Dexter'
 )
 ORDER BY name ASC;
