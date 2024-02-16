471af7c3abc8d81842dd3a87b09cfda471431f56
Sandy Obare <obaresandy@gmail.com>
2024-02-16 00:23:44 +0000

    10-genre_id_by_show.sql script initial commit A script that lists all shows in 'hbtn_0d_tvshows' database that have... at least one genre linked Columns displayed: tv_shows.title, tv_shows_genres.genre_id Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- A script that lists all shows in 'hbtn_0d_tvshows' database that have...
+-- at least one genre linked
+-- Columns displayed: tv_shows.title, tv_shows_genres.genre_id
+-- Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT ts.title, tsg.genre_id
+FROM tv_shows ts
+JOIN tv_show_genres tsg
+ON ts.id = tsg.show_id
+ORDER BY title, tsg.genre_id ASC
