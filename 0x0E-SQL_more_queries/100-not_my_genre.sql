b740bae56335e70cae2c04bf69e6a76c5ca87d95
Sandy Obare <obaresandy@gmail.com>
2024-02-16 23:11:13 +0000

    100-not_my_genre.sql script initial commit This script lists all genres not linked to the show 'Dexter' Each record displays the tv_genres.name column Results are sorted in ascending order by genre name

---
+++ 0x0E-SQL_more_queries/100-not_my_genre.sql
@@ -0,0 +1,12 @@
+-- This script lists all genres not linked to the show 'Dexter'
+-- Each record displays the tv_genres.name column
+-- Results are sorted in ascending order by genre name
+SELECT tg.name
+FROM tv_gernres tg
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM tv_show_genres tsg
+	JOIN tv_shows ts ON tsg.shows_id = ts.id
+	WHERE tsg.genre_id = tg.id AND ts.title = 'Dexter'
+)
+ORDER BY name ASC;
