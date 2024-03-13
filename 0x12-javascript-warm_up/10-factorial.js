2aea83446f5524edd61233c3a4033b53cede90c8
Sandy Obare <obaresandy@gmail.com>
2024-03-13 20:06:33 +0000

    10-factorial.js changing factorial function from an arrow function to named function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@
 const { argv } = require('node:process');
 const argNumber = parseInt(argv[2]);
 
-const factorial = (n) => {
+function factorial (n) {
   if (n <= 1 || !Number.isInteger(n)) return 1;
   return n * factorial(n - 1);
-};
+}
 
 const result = factorial(argNumber);
 console.log(result);
