e118e3f854c5f13fc85f56f1245b6de77ac303a5
Sandy Obare <obaresandy@gmail.com>
2024-03-13 12:10:35 +0000

    5-to_integer.js semistandard compliance

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-let argNumber = parseInt(argv[2])
+const argNumber = parseInt(argv[2]);
 
 if (!Number.isInteger(argNumber)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
   console.log(`My number: ${argNumber}`);
 }
