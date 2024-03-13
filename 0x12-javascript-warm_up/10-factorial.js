f1e6644b4f9acd7e59a896cb598e2084b1f3afa2
Sandy Obare <obaresandy@gmail.com>
2024-03-13 20:01:53 +0000

    10-factorial.js initial commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const argNumber = parseInt(argv[2]);
+
+const factorial = (n) => {
+  if (n <= 1 || !Number.isInteger(n)) return 1;
+  return n * factorial(n - 1);
+};
+
+const result = factorial(argNumber);
+console.log(result);
