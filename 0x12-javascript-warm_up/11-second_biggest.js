9e3da024706ffccf412eb2a07f8b25cd6956ffb2
Sandy Obare <obaresandy@gmail.com>
2024-06-17 19:32:57 +0300

    11-second_biggest.js script initial commit #1: Script searches for second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv.length === 2 || argv.length === 3) {
+  console.log(0);
+} else {
+  let largest = parseInt(argv[2]);
+  let second = parseInt(argv[2]);
+  argv.slice(2).forEach(element => {
+    if (parseInt(element) > largest) {
+      largest = parseInt(element);
+    }
+  });
+
+  argv.slice(2).forEach(element => {
+    if (parseInt(element) !== largest && parseInt(element) > second) {
+      second = parseInt(element);
+    }
+  });
+
+  console.log(second);
+}
