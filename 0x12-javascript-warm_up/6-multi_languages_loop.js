97d91052e7c205f7b7517400794eb6a46a4a5aec
Sandy Obare <obaresandy@gmail.com>
2024-06-17 16:04:34 +0300

    6-multi_languages_loop.js script initial commit #1 Script iterates through an array of strings and prints each string

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const statements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let count = 0; count < statements.length; count++) {
+  console.log(statements[count]);
+}
