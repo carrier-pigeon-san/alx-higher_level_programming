827d938800a7fb63e14e702877a2c7b8d013fd69
Sandy Obare <obaresandy@gmail.com>
2024-06-18 16:10:01 +0300

    6-square.js update #1 Changed superclass to 'Square' in 5-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { stdout } = require('process');
-const Rectangle = require('./4-rectangle.js');
+const Square1 = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends Square1 {
   constructor (size) {
     super(size, size);
   }
