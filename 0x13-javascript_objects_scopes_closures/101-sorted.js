4078e0b96d3211d6a319b4523ac60b2729a0c43c
Sandy Obare <obaresandy@gmail.com>
2024-06-20 00:24:23 +0300

    101-sorted.js initial commit Script imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence. The new dictionary is printed

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+const mutDict = {};
+
+for (const [key, value] of Object.entries(dict)) {
+  if (Object.hasOwn(mutDict, value)) {
+    mutDict[value] = mutDict[value].concat(key);
+  } else {
+    mutDict[value] = [key];
+  }
+}
+
+console.log(mutDict);
