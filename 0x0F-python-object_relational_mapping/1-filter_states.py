262baada80f020ab617989e72890a67418210fcc
Sandy Obare <obaresandy@gmail.com>
2024-06-21 11:27:59 +0300

    A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa: * The script takes 3 arguments: mysql username, mysql password and database name. * The script connects to a MySQL server running on localhost at port 3306. * Results are sorted in ascending order by states.id.

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains a script that list all states beginning with 'N' from
+database hbtn_0e_0_usa
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
+                         database=argv[3], port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT id, name FROM states\
+                WHERE REGEXP_LIKE(name, '^N') ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
