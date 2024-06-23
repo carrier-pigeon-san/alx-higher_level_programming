0a39e7dd40aade914b919ae54bdb550a0e1d793c
Sandy Obare <obaresandy@gmail.com>
2024-06-23 23:57:22 +0300

    11-model_state_insert.py initial commit A script that adds the State object “Louisiana” to the database hbtn_0e_6_usa: * Takes 3 arguments: mysql username, mysql password and database name * Uses the module SQLAlchemy * Imports State and Base from model_state - from model_state import Base, State * Connects to a MySQL server running on localhost at port 3306 * Prints the new states.id after creation

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A script that adds the State object “Louisiana” to the database hbtn_0e_6_usa:
+* Takes 3 arguments: mysql username, mysql password and database name
+* Uses the module SQLAlchemy
+* Imports State and Base from model_state - from model_state import Base, State
+* Connects to a MySQL server running on localhost at port 3306
+* Prints the new states.id after creation
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3]
+            ),
+        pool_pre_ping=True
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    la = State()
+    la.name = "Louisiana"
+    session.add(la)
+    session.commit()
+    query = session.query(State).order_by(State.id)
+    for s in query:
+        print("{}: {}".format(s.id, s.name))
