83741c3352e6b170f976da15881fdfcc4ed6d01d
Sandy Obare <obaresandy@gmail.com>
2024-07-04 15:41:27 +0300

    0-body_size.sh initial commit The script accepts a URL, to which it sends a request , and displays the... size of the body of the response in bytes

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# The script accepts a URL, to which it sends a request , and displays the...
+# size of the body of the response in bytes
+
+curl -sI "$1" | grep Content-Length | sed 's/Content-Length: //g'
