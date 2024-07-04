0f9c84b165b529e3839c9365356692919b9769c5
Sandy Obare <obaresandy@gmail.com>
2024-07-04 15:46:35 +0300

    0-body_size.sh update #2 Reduced number of lines in 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# The script accepts a URL, to which it sends a request , and displays the...
-# size of the body of the response in bytes
-
+# The script accepts a URL, to which it sends a request , and displays the size of the body of the response in bytes
 curl -sI "$1" | grep Content-Length | sed 's/Content-Length: //g'
