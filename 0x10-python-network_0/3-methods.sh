9f41ef767a66587bdb35404ceb3b8584f9a643de
Sandy Obare <obaresandy@gmail.com>
2024-07-04 16:28:45 +0300

    3-methods.sh update #1 Replaced URL in curl command with argv variable

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX OPTIONS 0.0.0.0:5000/route_4 | grep Allow | sed 's/Allow: //g'
+curl -sIX OPTIONS "$1" | grep Allow | sed 's/Allow: //g'
