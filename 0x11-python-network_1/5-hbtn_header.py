83b2da1a4b5e081d6d6ed83c070c2e9a44e083af
Sandy Obare <obaresandy@gmail.com>
2024-07-06 02:22:30 +0300

    5-hbtn_header.py initial commit This module contains a script that takes in a URL, sends a request to theURL and displays the value of the variable X-Request-Id in the responseheader

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains a script that takes in a URL, sends a request to the
+URL and displays the value of the variable `X-Request-Id in the response
+header"""
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    link = argv[1]
+    r = requests.get(link)
+    print(r.headers['X-Request-Id'])
