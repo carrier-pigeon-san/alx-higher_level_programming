1b934ac4481c5c4fb60d8d56c53b04bfa4119661
Sandy Obare <obaresandy@gmail.com>
2024-07-06 04:04:38 +0300

    8-json_api.py initial commit A script that takes in a letter and sends a POST request to a given URL with the letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains a script that takes in a letter and sends a POST
+request to `http://0.0.0.0:5000/search_user` with the letter as a parameter"""
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    link = 'http://0.0.0.0:5000/search_user'
+    q = argv[1] if len(argv) > 1 else ""
+    r = requests.post(link, data={'q': q})
+    try:
+        j = r.json()
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+    else:
+        if j:
+            print('[{}] {}'.format(j.get('id'), j.get('name')))
+        else:
+            print("No result")
